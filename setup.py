08b4a397c79da53b64e1c710c80a59d35c91da05
David M Fobes <pseudocubic@gmail.com>
2014-06-09 16:04:00 -0400

    Updated setup.py to auto-cythonize during install

--- setup.py
+++ setup.py
@@ -2,9 +2,20 @@
 
 from distutils.core import setup, Extension
 from distutils.sysconfig import get_python_inc, get_python_lib
-from kapteyn import __version__ as version
+from Cython.Distutils import build_ext
 from glob import glob
 import sys, os
+
+try:
+   import numpy
+except:
+   sys.exit(1)
+
+include_dirs = []
+numdir = os.path.dirname(numpy.__file__)
+ipath = os.path.join(numdir, numpy.get_include())
+include_dirs.append(ipath)
+include_dirs.append('src')
 
 setup(name='neutronpy',
       version='0.1',
@@ -15,10 +26,11 @@ setup(name='neutronpy',
       install_requires=['numpy>=1.8.1', 'six>=1.6.1'],
       packages=find_packages(),
       test_suite='neutronpy.tests.test_all',
+      cmdclass = {'build_ext': build_ext},
       ext_modules=[
       Extension(
          "kmpfit",
-         ["src/kmpfit.c", "src/mpfit.c"],
+         ["src/kmpfit.pyx", "src/mpfit.c"],
          include_dirs=include_dirs
       )
    ],
