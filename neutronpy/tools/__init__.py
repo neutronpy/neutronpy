8c428251bee6088798eb7810d220e452395fbafc
David M Fobes <pseudocubic@gmail.com>
2014-06-24 15:25:27 -0400

    Added Data class

--- neutronpy/tools/__init__.py
+++ neutronpy/tools/__init__.py
@@ -1,3 +1,4 @@
-from neutronpy.tools.Energy import Neutron
-from neutronpy.tools.Stitch import combine1D, combine2D
-from neutronpy.tools.Plot import cut1D, slice2D
+from .Energy import Neutron
+from .Data import Data
+from .Stitch import combine1D, combine2D
+from .Plot import cut1D, slice2D
