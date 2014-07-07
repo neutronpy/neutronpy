fa604c3f428dea64c8f716bb672ed77e060943ec
David M Fobes <pseudocubic@gmail.com>
2014-07-07 16:54:34 -0400

    Added Method Documentation

--- neutronpy/tools/Data.py
+++ neutronpy/tools/Data.py
@@ -1,17 +1,60 @@
+from ..constants import boltzmann_meV_K
 import numpy as np
 import matplotlib.pyplot as plt  # @UnusedImport
 from scipy.interpolate import griddata  # @UnusedImport
 
 
 class Data(object):
-    '''Data class for handling multi-dimensional TAS data
+    '''Data class for handling multi-dimensional TAS data. If input file type is not supported, data can be entered manually.
+
+    Parameters
+    ----------
+    h : ndarray, optional
+        Array of Q$_x$ in reciprocal lattice units.
+
+    k : ndarray, optional
+        Array of Q$_y$ in reciprocal lattice units.
+
+    l : ndarray, optional
+        Array of Q$_z$ in reciprocal lattice units.
+
+    e : ndarray, optional
+        Array of $\hbar \omega$ in meV.
+
+    detector : ndarray, optional
+        Array of measured counts on detector.
+
+    monitor : ndarray, optional
+        Array of measured counts on monitor.
+
+    temp : ndarray, optional
+        Array of sample temperatures in K.
+
+    Returns
+    -------
+    Data Class
+        The data class for handling Triple Axis Spectrometer Data
+
     '''
     def __init__(self, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
 
-    def load_file(self, *files, mode='', **kwargs):
+    def load_file(self, *files, mode, **kwargs):
         '''Loads one or more files in either HFIR or NCNR formats
+
+        Parameters
+        ----------
+        files : string
+            A file or non-keyworded list of files containing data for input.
+
+        mode : string
+            Specify file type (HFIR | NCNR). Currently only file types supported.
+
+        Returns
+        -------
+        None
+
         '''
         if mode == 'HFIR':
             keys = {'h': 'h', 'k': 'k', 'l': 'l', 'e': 'e', 'monitor': 'monitor', 'detector': 'detector', 'temp': 'temp'}
@@ -23,7 +66,7 @@ class Data(object):
                             *args, = next(f).split()  # @IgnorePep8
                             headers = [head.replace('.', '') for head in args[1:]]
 
-                *args, = np.loadtxt(filename, unpack=True, dtype=np.float64)  # @IgnorePep8
+                * args, = np.loadtxt(filename, unpack=True, dtype=np.float64)  # @IgnorePep8
 
                 for key, value in keys.items():
                     output[key] = args[headers.index(value)]
@@ -48,7 +91,7 @@ class Data(object):
                         if 'Q(x)' in line:
                             *args, = line.split()  # @IgnorePep8
                             headers = [head.replace('(', '').replace(')', '').replace('-', '') for head in args]
-                *args, = np.loadtxt(filename, unpack=True, dtype=np.float64, skiprows=12)  # @IgnorePep8
+                * args, = np.loadtxt(filename, unpack=True, dtype=np.float64, skiprows=12)  # @IgnorePep8
 
                 for key, value in keys.items():
                     output[key] = args[headers.index(value)]
@@ -62,7 +105,18 @@ class Data(object):
                     self.combine_data(output)
 
     def build_Q(self, **kwargs):
-        '''Internal class for constructing Q[q, hw] from h, k, l, and energy
+        '''Internal method for constructing Q[q, hw] from h, k, l, and energy
+
+        Parameters
+        ----------
+        output : dictionary, optional
+            A dictionary of the h, k, l, and e arrays to form into a column oriented array
+
+        Returns
+        -------
+        Q : ndarray, shape (N, 4,)
+            Returns Q (h, k, l, e) in a column oriented array.
+
         '''
         args = ()
         if hasattr(kwargs, 'output'):
@@ -75,7 +129,18 @@ class Data(object):
         return np.vstack((item.flatten() for item in args)).T
 
     def intensity(self, **kwargs):
-        '''Returns monitor normalized intensity
+        '''Returns the monitor normalized intensity
+
+        Parameters
+        ----------
+        m0 : float, optional
+            Desired monitor to normalize the intensity
+
+        Returns
+        -------
+        intensity : ndarray
+            The monitor normalized intensity scaled by m0
+
         '''
         try:
             m0 = kwargs['m0']
@@ -83,18 +148,64 @@ class Data(object):
             try:
                 m0 = self.m0
             except:
-                m0 = np.nanmax(self.monitor)
-                self.m0 = m0
+                self.m0 = m0 = np.nanmax(self.monitor)
 
         return self.detector / self.monitor * m0
 
     def error(self, **kwargs):
         '''Returns square-root error of monitor normalized intensity
+
+        Parameters
+        ----------
+        m0 : float, optional
+            Desired monitor to normalize the intensity
+
+        Returns
+        -------
+        error : ndarray
+            The square-root error of the monitor normalized intensity
+
         '''
         return np.sqrt(self.intensity(**kwargs))
 
+    def detailed_balance_factor(self, **kwargs):
+        '''Returns the detailed balance factor (sometimes called the Bose factor)
+
+        Parameters
+        ----------
+        temp : float, optional
+            If not already a property of the class, the sample temperature can be specified as a float.
+
+        Returns
+        -------
+        dbf : ndarray
+            The detailed balance factor (temperature correction)
+
+        '''
+        try:
+            self.temps = np.ones(self.Q.shape[0]) * kwargs['temp']
+        except:
+            pass
+
+        return np.exp(-self.Q[3] / boltzmann_meV_K / self.temps)
+
     def combine_data(self, *args, **kwargs):
         '''Combines multiple data sets
+
+        Parameters
+        ----------
+        args : dictionary of ndarrays
+            A dictionary (or multiple) of the data that will be added to the current data, with keys:
+                * Q : ndarray : [h, k, l, e] with shape (N, 4,)
+                * monitor : ndarray : shape (N,)
+                * detector : ndarray : shape (N,)
+                * temps : ndarray : shape (N,)
+
+        Returns
+        -------
+        None
+            Adds the data to the class, provides no returns.
+
         '''
         for arg in args:
             combine = []
@@ -143,21 +254,22 @@ class Data(object):
         Q = np.meshgrid(*q)
         Q = np.vstack((item.flatten() for item in Q)).T
 
-        monitor, detector, temp = [np.zeros(Q.shape[0])] * 3
+        monitor, detector, temps = [np.zeros(Q.shape[0])] * 3
 
         for i in range(Q.shape[0]):
             to_bin = np.where(np.abs((self.Q[:, 0] - Q[i, 0]) ** 2 / (qstep[0] / 2.) ** 2 +
                                      (self.Q[:, 1] - Q[i, 1]) ** 2 / (qstep[1] / 2.) ** 2 +
                                      (self.Q[:, 2] - Q[i, 2]) ** 2 / (qstep[2] / 2.) ** 2 +
-                                     (self.Q[:, 3] - Q[i, 3]) ** 2 / (qstep[3] / 2.) ** 2) < 1.)
+                                     (self.Q[:, 3] - Q[i, 3]) ** 2 / (qstep[3] / 2.) ** 2 +
+                                     (self.temps - Q[i, 4]) ** 2 / (qstep[4] / 2.)) < 1.)
             if to_bin[0]:
                 monitor[i] = np.average(self.monitor[to_bin])
                 detector[i] = np.average(self.detector[to_bin])
-                temp[i] = np.average(self.temp[to_bin])
+                temps[i] = np.average(self.temp[to_bin])
 
-        return Q, monitor, detector, temp
+        return Q, monitor, detector, temps
 
-    def integrate(self, bounds=None, **kwargs):
+    def integrate(self, **kwargs):
         '''Returns the integrated intensity within given bounds
         '''
         if hasattr(kwargs, 'bounds'):
@@ -166,12 +278,32 @@ class Data(object):
         else:
             np.sum()
 
-    def mean_squared_position(self, bounds=None, **kwargs):
+    def mean_squared_position(self, **kwargs):
         '''Returns the mean-squared position of a peak within the given bounds
         '''
-        pass
+        if hasattr(kwargs, 'bounds'):
+            tofit = np.where(kwargs['bounds'])
+            print(tofit)
+        else:
+            np.sum()
 
-    def width(self, bounds=None, **kwargs):
+    def width(self, **kwargs):
         '''Returns the width of a peak within the given bounds
         '''
-        pass
+        if hasattr(kwargs, 'bounds'):
+            tofit = np.where(kwargs['bounds'])
+            print(tofit)
+        else:
+            np.sum()
+
+    def plot(self, **kwargs):
+        axes = ['x', 'y', 'z', 'w']
+        options = ['h', 'k', 'l', 'e', 'temp', 'intensity']
+
+        to_bin = ()
+        for opt in options[:4]:
+            for axis in axes:
+                if getattr(kwargs, axis)[0] == opt:
+                    to_bin += (getattr(kwargs, axis)[1:],)
+
+        Q, monitor, detector, temps = self.bin(*to_bin)
