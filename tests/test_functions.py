4e5f4beec1249fddaf08b1e6600b11172357a7a7
David M Fobes <pseudocubic@gmail.com>
2014-08-04 15:50:50 -0400

    TESTS: Added test for gauss ring function

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -5,30 +5,34 @@ from scipy.integrate import simps
 
 
 class FunctionTests(unittest.TestCase):
-    def __init__(self, *args, **kwargs):
-        super(FunctionTests, self).__init__(*args, **kwargs)
-        self.x = np.linspace(-1e6, 1e6, 8e6 + 1)
-
     def test_gauss_norm(self):
         p = np.array([0., 0., 1., -30., 3., 1., 30., 3.])
-        y = functions.gaussian(p, self.x)
-        integ = simps(y, x=self.x)
+        x = np.linspace(-1e6, 1e6, 8e6 + 1)
+        y = functions.gaussian(p, x)
+        integ = simps(y, x)
         self.assertAlmostEqual(integ, 2., 5)
 
     def test_lorent_norm(self):
         p = np.array([0., 0., 1., -30., 3., 1., 30., 3.])
-        y = functions.lorentzian(p, self.x)
-        integ = simps(y, x=self.x)
+        x = np.linspace(-1e6, 1e6, 8e6 + 1)
+        y = functions.lorentzian(p, x)
+        integ = simps(y, x)
         self.assertAlmostEqual(integ, 2., 5)
 
     def test_voigt_norm(self):
         p = np.array([0., 0., 1., -30., 2., 3., 1., 30., 2., 3.])
-        y = functions.voigt(p, self.x)
-        integ = simps(y, x=self.x)
+        x = np.linspace(-1e6, 1e6, 8e6 + 1)
+        y = functions.voigt(p, x)
+        integ = simps(y, x)
         self.assertAlmostEqual(integ, 2., 5)
 
     def test_gaussring_norm(self):
-        pass
+        p = np.array([0., 0., 1., 0., 0., 0.5, 0.5, 0.1])
+        a, b = np.linspace(-10, 10, 1001), np.linspace(-10, 10, 1001)
+        q = np.meshgrid(a, b, sparse=True)
+        y = functions.gaussian_ring(p, q)
+        integ = simps(simps(y, b), a)
+        self.assertAlmostEqual(integ, 1., 5)
 
     def test_resolution_norm(self):
         p = np.array([0., 0., 1., 0., 0., 1.43, 23867.71, 22311.93, 20739.82])
