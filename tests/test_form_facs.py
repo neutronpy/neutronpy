5a4a1663f8a02ff984a9dccd994b0b02bf1f4ef1
David M Fobes <pseudocubic@gmail.com>
2014-08-04 16:07:09 -0400

    TESTS: Updated structure factor tests to generalize

--- tests/test_form_facs.py
+++ tests/test_form_facs.py
@@ -6,21 +6,19 @@ class StructureFactor(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(StructureFactor, self).__init__(*args, **kwargs)
 
-        self.input = {'name': 'Fe1.1Te',
+        self.input = {'name': 'FeTe',
                       'composition': [{'ion': 'Fe', 'pos': [0.75, 0.25, 0.]},
                                       {'ion': 'Fe', 'pos': [1. - 0.75, 1. - 0.25, 0.0]},
                                       {'ion': 'Te', 'pos': [0.25, 0.25, 1. - 0.2839]},
-                                      {'ion': 'Te', 'pos': [1. - 0.25, 1. - 0.25, 1. - (1. - 0.2839)]},
-                                      {'ion': 'Fe', 'pos': [0.25, 0.25, 0.721], 'occupancy': 0.1},
-                                      {'ion': 'Fe', 'pos': [1. - 0.25, 1. - 0.25, 1. - 0.721], 'occupancy': 0.1}],
-                      'debye-waller': False,
+                                      {'ion': 'Te', 'pos': [1. - 0.25, 1. - 0.25, 1. - (1. - 0.2839)]}],
+                      'debye-waller': True,
                       'massNorm': True,
-                      'formulaUnits': 2.,
+                      'formulaUnits': 1.,
                       'lattice': [3.81, 3.81, 6.25]}
 
     def test_str_fac(self):
         structure = form_facs.Material(self.input)
-        self.assertAlmostEqual(abs(structure.calc_str_fac((1., 1., 0.)) ** 2), 892301.46253218898, 6)
+        self.assertAlmostEqual(abs(structure.calc_str_fac((2., 0., 0.)) ** 2), 1583878.155915682, 6)
 
 
 class MagneticFormFactor(unittest.TestCase):
@@ -28,7 +26,7 @@ class MagneticFormFactor(unittest.TestCase):
         ion = form_facs.Ion('Fe')
 
         formfac, *_temp = ion.calc_mag_form_fac(q=1.)
-        self.assertAlmostEqual(formfac, 0.932565447328, 6)
+        self.assertAlmostEqual(formfac, 0.932565, 6)
 
 
 if __name__ == "__main__":
