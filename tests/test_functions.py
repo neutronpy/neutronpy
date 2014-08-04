e8f60c37fb556551261c85d2d1c52752e063534e
David M Fobes <pseudocubic@gmail.com>
2014-08-04 14:26:33 -0400

    TESTS: Fixed function.resolution test

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,40 +1,42 @@
 from neutronpy import functions
 import unittest
 import numpy as np
+from scipy.integrate import simps
 
 
 class FunctionTests(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(FunctionTests, self).__init__(*args, **kwargs)
-        self.x = np.linspace(-100000, 100000, 4000001)
+        self.x = np.linspace(-1e6, 1e6, 8e6 + 1)
 
     def test_gauss_norm(self):
         p = np.array([0., 0., 1., -30., 3., 1., 30., 3.])
         y = functions.gaussian(p, self.x)
-        integ = np.trapz(y, x=self.x)
-        self.assertAlmostEqual(integ, 2., 4)
+        integ = simps(y, x=self.x)
+        self.assertAlmostEqual(integ, 2., 5)
 
     def test_lorent_norm(self):
         p = np.array([0., 0., 1., -30., 3., 1., 30., 3.])
         y = functions.lorentzian(p, self.x)
-        integ = np.trapz(y, x=self.x)
-        self.assertAlmostEqual(integ, 2., 4)
+        integ = simps(y, x=self.x)
+        self.assertAlmostEqual(integ, 2., 5)
 
     def test_voigt_norm(self):
         p = np.array([0., 0., 1., -30., 2., 3., 1., 30., 2., 3.])
         y = functions.voigt(p, self.x)
-        integ = np.trapz(y, x=self.x)
-        self.assertAlmostEqual(integ, 2., 4)
+        integ = simps(y, x=self.x)
+        self.assertAlmostEqual(integ, 2., 5)
 
     def test_gaussring_norm(self):
         pass
 
     def test_resolution_norm(self):
         p = np.array([0., 0., 1., 0., 0., 1.43, 23867.71, 22311.93, 20739.82])
-        q = np.meshgrid(np.linspace(-10, 10, 2001), np.linspace(-10, 10, 2001))
+        a, b = np.linspace(-1, 1, 501), np.linspace(-1, 1, 501)
+        q = np.meshgrid(a, b, sparse=True)
         y = functions.resolution(p, q)
-        integ = np.trapz(y.flatten(), x=q[0].flatten()) + np.trapz(y.flatten(), x=q[1].flatten())
-        self.assertAlmostEqual(integ, 1., 4)
+        integ = simps(simps(y, b), a)
+        self.assertAlmostEqual(integ, 1., 5)
 
 
 if __name__ == '__main__':
