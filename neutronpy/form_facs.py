FIX: ComplexWarning in Form Factor calculation
Fixes #25

The form factor calculation produced the warning `ComplexWarning: Casting complex values to real discards the imaginary part`, due to the initial form factor array being the wrong dtype. It has been changed to complex64.

Tests were updated using the original IDL code which does not exhibit any of these type of casting issues.

neutronpy/form_facs.py
@@ -169,18 +169,18 @@ class Material(object):
  
         # Ensures input arrays are complex ndarrays
         if isinstance(h, (np.ndarray, list, tuple)):
-            h = np.array(h).astype(complex, casting='unsafe')
+            h = np.array(h).astype(complex)
         if isinstance(k, (np.ndarray, list, tuple)):
-            k = np.array(k).astype(complex, casting='unsafe')
+            k = np.array(k).astype(complex)
         if isinstance(l, (np.ndarray, list, tuple)):
-            l = np.array(l).astype(complex, casting='unsafe')
+            l = np.array(l).astype(complex)
 
         # Determines shape of input variables to build FQ = 0 array
         _dims = h + k + l
         if isinstance(_dims, Number):
             FQ = 0 * 1j
         else:
-            FQ = np.zeros(_dims.shape)
+            FQ = np.zeros(_dims.shape, dtype=np.complex)
 
         # construct structure factor
         for atom in self.atoms:

tests/test_form_facs.py
@@ -25,21 +25,21 @@ class StructureFactor(unittest.TestCase):
         self.assertAlmostEqual(np.abs(structure.calc_str_fac((0, 2, 0))) ** 2, 1583878.155915682, 6)
 
         ndarray_example = np.linspace(0.5, 1.5, 21)
-        self.assertAlmostEqual(np.sum(abs(structure.calc_str_fac((ndarray_example, 0, 0))) ** 2), 1261922.414836668, 6)
-        self.assertAlmostEqual(np.sum(abs(structure.calc_str_fac((0, ndarray_example, 0))) ** 2), 1261922.414836668, 6)
-        self.assertAlmostEqual(np.sum(abs(structure.calc_str_fac((0, 0, ndarray_example))) ** 2), 16294175.79743738, 6)
-        self.assertAlmostEqual(np.sum(abs(structure.calc_str_fac((ndarray_example, ndarray_example, 0))) ** 2), 5572585.110405569, 6)
+        self.assertAlmostEqual(np.sum(abs(structure.calc_str_fac((ndarray_example, 0, 0))) ** 2), 6913512.8842766033, 6)
+        self.assertAlmostEqual(np.sum(abs(structure.calc_str_fac((0, ndarray_example, 0))) ** 2), 6913512.8842766033, 6)
+        self.assertAlmostEqual(np.sum(abs(structure.calc_str_fac((0, 0, ndarray_example))) ** 2), 16702432.671269652, 6)
+        self.assertAlmostEqual(np.sum(abs(structure.calc_str_fac((ndarray_example, ndarray_example, 0))) ** 2), 9805385.2963885423, 6)
 
 
         list_example = list(ndarray_example)
-        self.assertAlmostEqual(np.sum(abs(structure.calc_str_fac((list_example, 0, 0))) ** 2), 1261922.414836668, 6)
-        self.assertAlmostEqual(np.sum(abs(structure.calc_str_fac((0, list_example, 0))) ** 2), 1261922.414836668, 6)
-        self.assertAlmostEqual(np.sum(abs(structure.calc_str_fac((0, 0, list_example))) ** 2), 16294175.79743738, 6)
+        self.assertAlmostEqual(np.sum(abs(structure.calc_str_fac((list_example, 0, 0))) ** 2), 6913512.8842766033, 6)
+        self.assertAlmostEqual(np.sum(abs(structure.calc_str_fac((0, list_example, 0))) ** 2), 6913512.8842766033, 6)
+        self.assertAlmostEqual(np.sum(abs(structure.calc_str_fac((0, 0, list_example))) ** 2), 16702432.671269652, 6)
         
         tuple_example = tuple(ndarray_example)
-        self.assertAlmostEqual(np.sum(abs(structure.calc_str_fac((tuple_example, 0, 0))) ** 2), 1261922.414836668, 6)
-        self.assertAlmostEqual(np.sum(abs(structure.calc_str_fac((0, tuple_example, 0))) ** 2), 1261922.414836668, 6)
-        self.assertAlmostEqual(np.sum(abs(structure.calc_str_fac((0, 0, tuple_example))) ** 2), 16294175.79743738, 6)
+        self.assertAlmostEqual(np.sum(abs(structure.calc_str_fac((tuple_example, 0, 0))) ** 2), 6913512.8842766033, 6)
+        self.assertAlmostEqual(np.sum(abs(structure.calc_str_fac((0, tuple_example, 0))) ** 2), 6913512.8842766033, 6)
+        self.assertAlmostEqual(np.sum(abs(structure.calc_str_fac((0, 0, tuple_example))) ** 2), 16702432.671269652, 6)
     
     def test_N_atoms(self):
         structure = form_facs.Material(self.input)