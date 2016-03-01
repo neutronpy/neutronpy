84d97318f222baf51a815eb11c181585b31d5cea
pseudocubic <pseudocubic@gmail.com>
2016-03-01 15:57:01 -0700

    ADD: Basic model equations
    Added model equations for acoustic phonon, ferromagnetic dispersion, antiferromagnetic dispersion, simple harmonic oscillator, and damped harmonic oscillator

--- neutronpy/models.py
+++ neutronpy/models.py
@@ -1,18 +1,25 @@
 r'''Physical Models
 
 '''
+import numpy as np
 
 
-def harmonic_oscillator():
-    r'''Standard equation for a harmonic oscillator
+def simple_harmonic_oscillator(p, t):
+    r'''Standard equation for a simple harmonic oscillator
     '''
-    pass
+    return p[0] + p[1] * np.cos(p[2] * t - p[3])
 
 
-def acoustic_phonon_dispersion():
+def damped_harmonic_oscillator(p,t):
+    r'''Standard equation for a damped harmonic oscillator
+    '''
+    return np.exp(-p[4] * t / 2) * simple_harmonic_oscillator(p, t)
+
+
+def acoustic_phonon_dispersion(p, x):
     r'''Standard equation for the dispersion of an acoustic phonon
     '''
-    pass
+    return np.sqrt(4 * p[0]) * np.abs(np.sin(p[1] * x))
 
 
 def optical_phonon_disperions():
@@ -21,13 +28,13 @@ def optical_phonon_disperions():
     pass
 
 
-def ferromagnetic_disperions():
+def ferromagnetic_disperions(p, x):
     r'''Standard equation for the dispersion of a ferromagnetic spin excitation
     '''
-    pass
+    return 4 * p[0] * (1 - np.cos(p[1] * x))
 
 
-def antiferromagnetic_disperions():
+def antiferromagnetic_disperions(p, x):
     r'''Standard equation for the dispersion of an antiferromagnetic spin excitation
     '''
-    pass
+    return p[0] * np.abs(np.sin(p[1] * x))
