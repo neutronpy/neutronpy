FIX: Calculation of R0 prefactor for resolution
Resolves #57

Incorrect propagation of FWHM to gaussian RMS width conversion factor fixed.

Incorrect matrix indices during calculation with sample mosaics.

Went back to Matlab ResLib code to generate new tests for a more simple case. More tests will need to be generated.

neutronpy/instrument_tools.py
@@ -74,13 +74,13 @@ def ellipse(saxis1, saxis2, phi=0, origin=None, npts=31):
 
 
 def get_bragg_widths(RM):
-    bragg = [np.sqrt(8 * np.log(2)) / np.sqrt(RM[0, 0]),
-             np.sqrt(8 * np.log(2)) / np.sqrt(RM[1, 1]),
-             np.sqrt(8 * np.log(2)) / np.sqrt(RM[2, 2]),
-             get_phonon_width(0, RM, [0, 0, 0, 1])[1],
-             np.sqrt(8 * np.log(2)) / np.sqrt(RM[3, 3])]
+    bragg = np.array([np.sqrt(8 * np.log(2)) / np.sqrt(RM[0, 0]),
+                      np.sqrt(8 * np.log(2)) / np.sqrt(RM[1, 1]),
+                      np.sqrt(8 * np.log(2)) / np.sqrt(RM[2, 2]),
+                      get_phonon_width(0, RM, [0, 0, 0, 1])[1],
+                      np.sqrt(8 * np.log(2)) / np.sqrt(RM[3, 3])])
 
-    return bragg
+    return bragg * 2
 
 
 def get_phonon_width(r0, M, C):

tests/test_resolution.py
@@ -195,7 +195,7 @@ class ResolutionTest(unittest.TestCase):
         NP = EXP.RMS
         R = EXP.R0
         BraggWidths = get_bragg_widths(NP)
-        angles = self.EXP_popovici.get_angles_and_Q(hkle)[0]
+        angles = self.EXP_coopernathans.get_angles_and_Q(hkle)[0]
         ResVol = (2 * np.pi) ** 2 / np.sqrt(np.linalg.det(NP)) * 2
 
         self.assertTrue(np.all(np.abs((RMS - NP)) < 100))