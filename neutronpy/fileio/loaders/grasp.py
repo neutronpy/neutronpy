0d1fff755d6503a3cb15b64b8f4e612b67fe7cf1
pseudocubic <pseudocubic@gmail.com>
2016-04-18 16:38:01 -0600

    ADD: Support for GRASP nxs files
    Added support for GRASP nxs file types

--- neutronpy/fileio/loaders/grasp.py
+++ neutronpy/fileio/loaders/grasp.py
@@ -25,17 +25,37 @@ class Grasp(Data):
         '''
         pass
 
-    def determine_subtype(self):
-        pass
+    def determine_subtype(self, filename):
+        with open(file) as f:
+            first_line = f.readline()
+            if filename[-3:].lower() == 'nxs' or 'HDF' in first_line:
+                return 'nxs'
+            if filename[-3:].lower() == 'dat' or 'GRASP' in first_line:
+                return 'dat'
 
-    def load_iexy(self):
-        pass
+    def load_nxs(self, filename):
+        import h5py
 
-    def load_spe(self):
-        pass
+        f = h5py.File(filename)
 
-    def load_xyie(self):
-        pass
+        intensity = np.squeeze(np.array(f['entry0/data1/intensity1']))
+        qx = np.squeeze(np.array(f['entry0/data1/qx1']))
+        qy = np.squeeze(np.array(f['entry0/data1/qy1']))
+        qangles = np.squeeze(np.array(f['entry0/data1/qangle1']))
+        mod_q = np.squeeze(np.array(f['entry0/data1/mod_q1']))
+        err_intensity = np.squeeze(np.array(f['entry0/data1/err_intensity1']))
 
-    def load_xye(self):
+        self._data = OrderedDict(intensity=intensity,
+                                 qx=qx,
+                                 qy=qy,
+                                 qangles=qangles,
+                                 mod_q=mod_q,
+                                 err_intensity=err_intensity,
+                                 monitor=np.ones(intensity.shape),
+                                 time=np.ones(intensity.shape))
+
+        self.data_keys = {'intensity': 'intensity', 'monitor': 'monitor', 'time': 'time'}
+        self._err = err_intensity
+
+    def load_dat(self):
         pass
