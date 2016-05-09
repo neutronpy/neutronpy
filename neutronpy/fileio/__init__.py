1ccfb8842c68a425a8f6eaa925ed35af6b75cbc1
David M Fobes <pseudocubic@gmail.com>
2016-05-09 14:08:51 -0600

    REF: Removed unused code
    Removed depreciated build_Q function (unused)

--- neutronpy/fileio/__init__.py
+++ neutronpy/fileio/__init__.py
@@ -1,5 +1,3 @@
 from . import loaders
-from . import tools
 from .data import load_data, detect_filetype, save_data
 from .instrument import load_instrument, save_instrument
-from .tools import build_Q
