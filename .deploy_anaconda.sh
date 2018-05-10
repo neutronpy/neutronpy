add1f0bf24960506eb5f4baabc47fe54ee6ebdf1
David M Fobes <dfobes@lanl.gov>
2018-05-09 21:50:14 -0600

    FIX: add neutronpy conda channel to deploy script

--- .deploy_anaconda.sh
+++ .deploy_anaconda.sh
@@ -7,6 +7,7 @@ export PATH=$HOME/miniconda/bin:$PATH
 conda update --yes -q conda
 conda config --set always_yes true
 conda config --set anaconda_upload yes
+conda config --add channels neutronpy
 
 conda install -q python=$TRAVIS_PYTHON_VERSION pip conda-build anaconda-client
 conda build --user neutronpy --token $CONDA_TOKEN ./conda-recipe
