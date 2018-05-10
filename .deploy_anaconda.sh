9bf203b1eb8d19b3d6818ee1dd2706323325e04d
David M Fobes <dfobes@lanl.gov>
2018-05-09 21:33:52 -0600

    FIX: Travis Deploy 2

--- .deploy_anaconda.sh
+++ .deploy_anaconda.sh
@@ -1,7 +1,7 @@
 set -e
 
-wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh
-bash miniconda.sh -b -p $home/miniconda
+wget https://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh
+bash miniconda.sh -b -p $HOME/miniconda
 export PATH=$HOME/miniconda/bin:$PATH
 
 conda update --yes -q conda
